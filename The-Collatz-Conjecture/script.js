824f1242a4d504d2480038783c4c09ecdb451fe4
mranxiety <mcleish.john+mranxiety@gmail.com>
2025-03-22 00:15:41 +1100

    Refactor script.js 2 electric boogaloo
    Refactored the script again so that calculating the list and displaying the list happen at the same time instead of as two distinct steps.
    
    Split out the creation of elements not related to calculating the list of numbers into their own functions which are called as needed.

--- The-Collatz-Conjecture/script.js
+++ The-Collatz-Conjecture/script.js
@@ -3,55 +3,51 @@
 // odd numbers multiply by 3 and add 1
 // even numbers divide by 2
 
-function collatz(){
-    //Creates elements for the legend
-    var oddText = document.createElement("p");
-    oddText.innerHTML = "Odd Numbers, ";
-    var evenText = document.createElement("p");
-    evenText.innerHTML = " Even Numbers";
-    oddText.className = "odd";
-    oddText.id = "oddLegend";
-    evenText.className = "even";
-    evenText.id = "evenLegend";
+function elementPrep(content, className, id){
+        //Creates elements for the odd and even numbers or text to be stored in.
+        let text = document.createElement("p");
+        if (className == "odd"){
+            text.className = "odd";
+        }
+        else {
+            text.className = "even";
+        }
+        if(id != null){
+            text.id = id; //"oddLegend" or "evenLegend"
+        }
+        text.innerHTML = content;
+        return text;
+}
 
-    //Gets the number from the input and does the collatz algorithm on it. Stores the numbers in an array.
-    var num = document.getElementById("number").value;
-    var arr = [];
+function steps(length){
+    let steps = document.getElementById("steps");
+    steps.innerHTML = `Steps to One: ${length}`;
+    steps.appendChild(elementPrep("Odd Numbers, ", "odd", "oddLegend"));
+    steps.firstElementChild.appendChild(elementPrep("Even Numbers, ", "even", "evenLegend")); //The legend explaining the colour code is two p elements. Appending one to the other means they will display inline while allowing me to style them indipendently.
+}
+
+function collatz(){
+    //Gets the number from the input, does the collatz algorithm on it, formats that to display on the page. Stores the numbers in an array for the steps.
+    let output = document.getElementById("output");
+    let num = document.getElementById("number").value;
+    output.innerHTML = ""; //clears the output for the next number, assuming the user is trying another number.
+    let arr = []; //Array to store the numbers in the sequence
     arr.push(num);
     console.log(num);
     if(num > 0){
         while (num != 1){ //If you don't have the loop break when it reaches 1, it will infinitely loop through 1, 4, 2, 1.
             if(num % 2 == 0) {
                 num = num / 2;
+                output.appendChild(elementPrep(num, "even", null));
             }
             else {
                 num = num * 3 + 1;
+                output.appendChild(elementPrep(num, "odd", null));
             }
             console.log(num); //For debugging
             arr.push(num);
         }
-
-        //Output the numbers into a list from the array and the steps it took to get to 1
-        var output = document.getElementById("output");
-        var steps = document.getElementById("steps");
-        steps.innerHTML = `Steps to One: ${arr.length}`;
-        steps.appendChild(oddText);
-        oddText.appendChild(evenText);
-        output.innerHTML = ""; //clears the output for the next number, assuming the user is trying another number.
-       arr.forEach(n => {
-        if(n % 2 == 0) {
-            var eP = document.createElement("p");
-            eP.className = "even";
-            eP.innerHTML = n;
-            output.appendChild(eP);
-        }
-        else {
-            var oP = document.createElement("p");
-            oP.className = "odd";
-            oP.innerHTML = n;
-            output.appendChild(oP);
-        }
-       })
+        steps(arr.length);
     }
     else {
         output.innerHTML = "Please enter a postive number or a number other than 0"; //Error message for negative numbers, 0, or no input
