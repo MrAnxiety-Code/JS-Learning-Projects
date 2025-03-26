6c10c77114732fd00c2608e4b94c420a2249874f
mranxiety <mcleish.john+mranxiety@gmail.com>
2025-03-26 23:29:54 +1100

    Change from Array to Map
    Changed from storing data in an array to a Map. This was the original plan, I was just not certain how to go about it originally. Now, after some experimenting, I know how to dynamically generate a key and store both the text and the state of the checkbox.
    
    Note: I'll need to set up a function to slice the two when I retrieve them.

--- Interactive-To-Do-List/script.js
+++ Interactive-To-Do-List/script.js
@@ -1,36 +1,44 @@
 var toDoList = document.getElementById('toDoList');
-var taskArray = [];
+var taskMap = new Map();
 
 function nameTask() {
+    //Create a new list item for the new task
     let li = document.createElement('li');
     toDoList.appendChild(li);
+    //Textbox for input
     let task = document.createElement('input');
     task.type = 'text';
     task.id = 'taskTextBox';
     li.appendChild(task);
+    //Button for saving the task
     let saveTask = document.createElement('button');
     saveTask.innerHTML = 'Save';
     saveTask.id = 'saveNewTaskButton';
-    li.appendChild(saveTask);
     saveTask.addEventListener('click', function(){addTask(task.value);});
+    li.appendChild(saveTask);
 }
 
 function addTask(task){
     if(task != ''){
         let li = toDoList.lastElementChild;
+        //Remove the textbox and save button
         let taskTextBox = document.getElementById('taskTextBox');
         let saveNewTaskButton = document.getElementById('saveNewTaskButton');
         taskTextBox.remove();
         saveNewTaskButton.remove();
+        //Create a checkbox for the task
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        checkbox.id = `task${taskArray.length}`;
-        checkbox.htmlfor = `task${taskArray.length}`;
+        checkbox.id = `task${taskMap.size}`;
+        checkbox.addEventListener('click', function(){taskMap.set(checkbox.id,`${checkbox.checked}${task}`);});
         li.appendChild(checkbox);
+        //Create a label for the task, this is what stores the task
         let label = document.createElement('label');
+        label.htmlfor = `task${taskMap.size}`;
         label.innerHTML = task;
         li.appendChild(label);
-        taskArray.push(task);
+        //Add the task to the map
+        taskMap.set(taskMap.size,`${checkbox.checked}${task}`);
     }
     else {
         return; //do nothing if the task is empty
@@ -39,5 +47,5 @@ function addTask(task){
 
 function clearAllTasks(){
     toDoList.replaceChildren();
-    taskArray = [];
+    taskMap.clear();
 }
